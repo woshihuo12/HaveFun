74b49382b511abc1fa9fa0a867820495ccb63aa4
woshihuo12 <253268499@qq.com>
2014-10-25 14:42:53 +0800

    尝试不同种layer的创建方式

--- hebut/src/app/MyApp.lua
+++ hebut/src/app/MyApp.lua
@@ -11,7 +11,7 @@ end
 
 function MyApp:run()
     cc.FileUtils:getInstance():addSearchPath("res/")
-    -- self:enterScene("login.scene")
+    -- self:enterScene("MainScene")
     SwitchScene("login")
 end
 
--- hebut/src/app/scenes/login/scene.lua
+++ hebut/src/app/scenes/login/scene.lua
@@ -1,33 +1,53 @@
+--[[
+    背景Layer
+--]]
+local bgLayer = class("bgLayer", function ()
+    return display.newLayer()
+end)
+
+function bgLayer:ctor()
+    display.newSprite("#bg.jpg")
+        :align(display.CENTER, display.cx, display.cy)
+        :addTo(self)
+end
+
+--[[
+    用户名密码Layer
+--]]
+local NameAndPasswdLayer = {}
+function NameAndPasswdLayer:new()
+    local this = {}
+    setmetatable(this , self)
+    self.__index = self
+
+    this.layer = display.newLayer()
+
+    local enterBtn = cc.ui.UIPushButton.new({normal = "#select_btn.png", pressed = "#select_btn_pre.png"})
+        :align(display.CENTER, display.cx, display.cy - 350)
+        :addTo(this.layer)
+        :setButtonLabel(cc.ui.UILabel.new({text = "进入游戏", color = cc.c3b(255,255,255)}))
+        :onButtonClicked(function ()
+            print("hello world")
+        end)
+
+    local sequence = transition.sequence({
+                cc.ScaleTo:create(0.5, 1.2,1.2),
+                cc.ScaleTo:create(0.5, 1, 1),
+            })
+    enterBtn:runAction(cc.RepeatForever:create(sequence))
+
+    return this.layer
+end
+
 
 local scene = class("scene", function()
     return display.newScene("scene")
 end)
 
 function scene:ctor()
-    -- cc.ui.UILabel.new({
-    --         UILabelType = 2, text = "Hello, World", size = 64})
-    --     :align(display.CENTER, display.cx, display.cy)
-    --     :addTo(self)
-    	-- 去掉所有未完成的动作
-	cc.Director:getInstance():getActionManager():removeAllActions()
     display.addSpriteFrames(IMAGE_SCENES_PATH .. "login/loginScene.plist", IMAGE_SCENES_PATH .. "login/loginScene.png")
-    display.newSprite("#bg.jpg")
-    		:align(display.CENTER, display.cx, display.cy)
-    		:addTo(self)
-
-    local enterBtn = cc.ui.UIPushButton.new({normal = "#select_btn.png", pressed = "#select_btn_pre.png"})
-    	:align(display.CENTER, display.cx, display.cy - 350)
-    	:addTo(self)
-    	:setButtonLabel(cc.ui.UILabel.new({text = "进入游戏", color = cc.c3b(255,255,255)}))
-    	:onButtonClicked(function ()
-    		print("hello world")
-    	end)
-
-    local sequence = transition.sequence({
-			    cc.ScaleTo:create(0.5, 1.2,1.2),
-			    cc.ScaleTo:create(0.5, 1, 1),
-			})
-	enterBtn:runAction(cc.RepeatForever:create(sequence))
+    bgLayer.new():addTo(self)
+    NameAndPasswdLayer:new():addTo(self)
 end
 
 function scene:onEnter()
@@ -35,6 +55,7 @@ function scene:onEnter()
 end
 
 function scene:onExit()
+    display.removeSpriteFramesWithFile(IMAGE_SCENES_PATH .. "login/loginScene.plist", IMAGE_SCENES_PATH .. "login/loginScene.png")
 end
 
 return scene
